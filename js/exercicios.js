da8ae0bb914c948a7b3a5db9b261ab4975bb3d1c
Leonardo Marques Pereira das Neves <ledomarques@gmail.com>
2021-03-22 10:10:08 -0300

    add exercícios e outros testes

---
+++ js/exercicios.js
@@ -0,0 +1,43 @@
+/*Comentário*/
+/*var nome = "Leonardo Marques";
+var idade = 38;
+var frase = "Inverno é melhor que Verão.";
+alert("Bem-vindo, " + nome.toUpperCase() + "! \n\tPelo jeito você tem " + idade + " anos.");
+alert(frase.replace("Inverno", "Primavera"));*/
+
+/*var lista = ["maçã", "banana", "mamão"];
+lista.push("uva");
+alert(lista);
+alert(lista.reverse());
+alert(lista.join(" | "));
+
+var fruta = {nome: "maçã", cor: "vermelha"};
+alert(fruta.nome);*/
+
+/*var idade = prompt("Qual sua idade?");
+if (idade >=18){
+    alert("Ok, você é maior de idade.");
+} else {
+    alert("Desculpe, mas você é menor de idade.");
+};*/
+
+/*var count = 0;
+while (count <= 5){
+    console.log(count)/
+    count++;
+};*/
+
+/*var count = 0;
+for(count=0; count<=10; count++){
+    console.log(count);
+};*/
+
+/*var d = new Date();
+alert(d.getMonth()+1);
+alert(d.getDay());
+console.log("Hoje é " + d.getDate() + "/" + (d.getMonth()+1));*/
+
+/*var trajeto = ["mãe", "floresta", "avó"];
+console.log(trajeto.join(">"));
+console.log(trajeto.toString());*/
+
--- js/main.js
+++ js/main.js
@@ -1,6 +1,32 @@
-/*Comentário*/
-var nome = "Leonardo Marques";
-var idade = 38;
-var frase = "Inverno é melhor que Verão.";
-alert("Bem-vindo, " + nome.toUpperCase() + "! \n\tPelo jeito você tem " + idade + " anos.");
-alert(frase.replace("Inverno", "Primavera"));
+function load(){
+    alert("Página carregada!");
+};
+
+function botao(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar!";    
+};
+
+function redirecionar(){
+    window.open("https://globallabs.academy/"); /*abre link em nova aba*/
+    window.location.href = "https://globallabs.academy/"; /*abre link na mesma página*/
+};
+
+function trocar(){
+    document.getElementById("mouse").innerHTML = "Obrigado por passar o mouse.";
+};
+
+function voltar(){
+    document.getElementById("mouse").innerHTML = "Passe o mouse aqui";
+}
+
+function trocar1(elemento){
+    elemento.innerHTML = "Obrigado por passar o mouse.";
+};
+
+function voltar1(elemento){
+    elemento.innerHTML = "Passe o mouse aqui";
+};
+
+function change(elemento){
+    console.log(elemento.value);
+}
